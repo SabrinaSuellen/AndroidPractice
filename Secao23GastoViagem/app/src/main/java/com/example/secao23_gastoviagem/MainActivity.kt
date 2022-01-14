95b92986aab4a1430250b270627694ae476913fe
SabrinaSuellen <sabrinasuellen1@gmail.com>
2022-01-14 08:07:05 -0300

    Finalizado Seção 23

--- Secao23GastoViagem/app/src/main/java/com/example/secao23_gastoviagem/MainActivity.kt
+++ Secao23GastoViagem/app/src/main/java/com/example/secao23_gastoviagem/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.secao23_gastoviagem
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
+import java.lang.NumberFormatException
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,17 +21,21 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         calculate()
     }
 
-    private fun calculate() {
-        val distance = input_distancia.text.toString().toFloat()
-        val preco = input_preco.text.toString().toFloat()
-        val autonomy = input_autonomia.text.toString().toFloat()
-        var value: Float = 0.0f
+    private fun calculate() = if (validation()) {
+        try {
+            val distance = input_distancia.text.toString().toFloat()
+            val preco = input_preco.text.toString().toFloat()
+            val autonomy = input_autonomia.text.toString().toFloat()
+            val value = (distance * preco) / autonomy
+            textview_totalrs.text = "R$ ${"%.2f".format(value)}"
 
-        if(autonomy > 0)
-            value = (distance * preco) / autonomy
-        textview_totalrs.text = "R$ ${"%.2f".format(value)}"
+        } catch (nfe: NumberFormatException) {
+            Toast.makeText(this, getString(R.string.informe_valor), Toast.LENGTH_LONG).show()
+        }
+    } else
+        Toast.makeText(this, getString(R.string.preencha_campos), Toast.LENGTH_LONG).show()
 
-
-    }
+    private fun validation(): Boolean = input_distancia.text.toString() != ""
+            && input_preco.text.toString() != "" && input_autonomia.text.toString() != ""
 
 }
