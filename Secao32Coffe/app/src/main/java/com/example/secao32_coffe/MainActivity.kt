9af1bfd5d010a20859af8655183c46dce2161e5b
SabrinaSuellen <sabrinasuellen1@gmail.com>
2022-01-20 11:17:40 -0300

    20/01/22 Seção 32 finalizada

---
+++ Secao32Coffe/app/src/main/java/com/example/secao32_coffe/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.secao32_coffe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if(supportActionBar != null)
+            supportActionBar!!.hide()
+    }
+}
